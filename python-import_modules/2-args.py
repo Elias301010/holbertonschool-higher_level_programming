d349e7ac4fac73acdf4c826154d0ab04887301c6
Elias301010 <elias301010@gmail.com>
2025-07-10 11:44:59 +0400

    zesxrdctfvygbuhnijmok

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     arg_cont = lens(sys.argv) - 1
     if arg_count ==0:
-        prinnt("0 arguments.")
+        print("0 arguments.")
     elif arg_count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(arg count))
+        print("{} arguments:".format(arg_count))
         for i in range(1, len(sys.argv))
             print("{}: {}".format(i, sys.argv[i]))
