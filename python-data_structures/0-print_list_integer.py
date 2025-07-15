f1e540082515f8a8e56184f7550e2850b4c5286b
Elias301010 <elias301010@gmail.com>
2025-07-15 13:02:09 +0400

    wxecrftvtgbyhunj

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -6,3 +6,6 @@ def element_at(my_list, idx):
     elif idx >= len(my_list):
         return None
     return my_list[idx]
+# Example usage:
+my_list = [1, 2, 3, 4, 5]
+print("Element at index {:d} is {}".format(3, element_at(my_list, 3)))
