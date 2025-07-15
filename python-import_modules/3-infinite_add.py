1c8c0a4155a8b2f9fed1467fd70ef70e62316247
Elias301010 <elias301010@gmail.com>
2025-07-15 10:10:19 +0400

    zdxcfvgbhnj

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ import sys
 if __name__ == "__main__":
     total = 0
     for arg in sys.argv[1:]:
-            total += int(arg)
+        total += int(arg)
     print(total)
-    #commitinging
+    # commitinging
